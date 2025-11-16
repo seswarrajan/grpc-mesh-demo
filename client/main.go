1af23a315fe83cbb8a7c31086ed5fde45831a266
Eswar Rajan <eswar@accuknox.com>
2025-11-16 23:26:27 +0530

    Loop request every 1 min

--- client/main.go
+++ client/main.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"time"
 
+	gcron "github.com/go-co-op/gocron"
 	pb "github.com/seswarrajan/grpc-mesh-demo/proto"
 	"google.golang.org/grpc"
+)
+
+var (
+	client pb.PaymentsServiceClient
+	ctx    context.Context
+	cancel context.CancelFunc
+	count  float64
 )
 
 func main() {
@@ -18,19 +27,31 @@ func main() {
 	}
 	defer conn.Close()
 
-	client := pb.NewPaymentsServiceClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	client = pb.NewPaymentsServiceClient(conn)
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	req := &pb.PaymentRequest{
-		OrderId:  "ORD-1001",
-		Amount:   199.99,
-		Currency: "USD",
+	schedule := gcron.NewScheduler(time.UTC)
+	schedule.SingletonModeAll()
+
+	_, err = schedule.Every("1m").Do(func() {
+		count = count + 1
+		resp, err := client.ProcessPayment(ctx, &pb.PaymentRequest{
+			OrderId:  "ORD-" + fmt.Sprintf("%f", count),
+			Amount:   count,
+			Currency: "USD",
+		})
+		if err != nil {
+			log.Fatalf("ProcessPayment error: %v", err)
+		}
+		log.Printf("Payment result: status=%s txn=%s", resp.Status, resp.TransactionId)
+	})
+	if err != nil {
+		log.Printf("\nerror in starting scheduler :%v\n", err)
+		return
 	}
 
-	resp, err := client.ProcessPayment(ctx, req)
-	if err != nil {
-		log.Fatalf("ProcessPayment error: %v", err)
-	}
-	log.Printf("Payment result: status=%s txn=%s", resp.Status, resp.TransactionId)
+	schedule.StartBlocking()
+
+	select {} /// Forever loop
 }
